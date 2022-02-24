4c69032fef7ab1aaaceeba5511aa4cd42af571f8
Tanya Kalenik <tmolekyla@gmail.com>
2022-02-25 02:40:13 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openBrackets = [];
+  let closeBrackets = [];
+  let strArr = [...str];
+  let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  strArr.forEach(function (el) {
+    if (
+      openBrackets.indexOf(el) >= 0 &&
+      (closeBrackets.indexOf(el) < 0 || stack.indexOf(el) < 0)
+    ) {
+      stack.push(el);
+      return;
+    }
+    if (
+      closeBrackets.indexOf(el) >= 0 &&
+      openBrackets[closeBrackets.indexOf(el)] === stack[stack.length - 1]
+    ) {
+      stack.pop();
+      return;
+    } else {
+      stack.push(el);
+    }
+  });
+  return stack.length === 0;
+};
